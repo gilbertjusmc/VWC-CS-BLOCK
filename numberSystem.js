bc361dd2a593404a70cbc14bab91496eff466a2a
gilbertjusmc <jgilbert1341@gmail.com>
2019-12-22 23:30:57 -0500

    started numberSystem

---
+++ numberSystem.js
@@ -0,0 +1,16 @@
+/*
+Assignment:
+1. Design your own class / function / etc. IE., Class called NumberSystem()
+2. It should be able to take in a integer or string IE., 134, “F8A5”, “1011", etc.
+3. It should convert numbers from binary to decimal, and hex to decimal.
+4. Look up how to convert decimal into hex and decimal to binary, add that to your class / program.
+
+Bonus:
+- Do Hex -> Binary
+- Trivial method? Hex -> Decimal -> Binary
+- Or you can find the mathematical shortcut, up to you how much time you want to spend
+*/
+
+function NumberSystem() {
+
+}
